fad4b3ed4c0ad52249130d541ca6a51877a25e7c
Brenden Hartman <brendenhart22@gmail.com>
2023-11-28 01:08:04 -0600

    Competed all test using object functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {name: "", streetAdress: ""};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    let newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
